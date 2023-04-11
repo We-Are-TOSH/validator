76e706b9481cf04beb2e480c2d3ea6af61564a84
Thabang Phaleng <thabangphaleng@Thabangs-MacBook-Pro.local>
2023-04-11 08:07:17 +0200

    [config] cors fixes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001' # Replace with the domain of your React app
+    origins '*' # Replace with the domain of your React app
     resource '*', headers: :any, methods: [:get, :post, :options]
   end
 end
